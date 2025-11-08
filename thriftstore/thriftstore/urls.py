60c7bfe67f158970f76e1e90cf792760058066ef
joeyNnamdi <dejoe4me@gmail.com>
2025-11-08 18:44:23 +0200

    update

--- thriftstore/thriftstore/urls.py
+++ thriftstore/thriftstore/urls.py
@@ -1,11 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('pudo/lockers/', views.pudo_lockers, name='pudo_lockers'),
-    path('pudo/quote/', views.pudo_quote, name='pudo_quote'),
-    path('checkout/confirm/<int:order_id>/', views.checkout_confirm, name='checkout_confirm'),
-    path('', include('shop.urls')),
-
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),  # ✅ include your shop app URLs
 ]
